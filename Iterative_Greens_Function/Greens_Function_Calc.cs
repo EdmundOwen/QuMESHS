c787e782285a7fa22f33688379bd5deeabf2f6bf
Edmund Owen <none@none>
2013-05-30 14:14:22 +0100

    Framework for Green's function iterator (not tested)

--- Iterative_Greens_Function/Greens_Function_Calc.cs
+++ Iterative_Greens_Function/Greens_Function_Calc.cs
@@ -10,6 +10,9 @@ namespace Iterative_Greens_Function
     {
         int no_Energy_Count, no_Slices;
         double dE;
+        Mem hop_mat, hop_mat_conj, H0_slice;
+
+        int slice_width;
 
         public void Initialise()
         {
@@ -27,8 +30,9 @@ namespace Iterative_Greens_Function
             for (int j = 1; j < no_Slices - 1; j++)
                 Iterate_Slice(ref G_ii, ref G_in, ref G_ni, Potential, j, Energy);
 
-            // Add output lead
-            Calculate_Connections(G_ii[no_Slices - 1], ref G_ii, ref G_in, ref G_ni, Potential, j, Energy);
+            // Add output lead (the Green's function for this slice was calculated
+            // with the boundary conditions)
+            Calculate_Connections(G_ii[no_Slices - 1], ref G_ii, ref G_in, ref G_ni, Potential, no_Slices - 1, Energy);
 
             // copy G_0n and G_n0 to the signiture inputs for current calculations
             G_0n = G_in[0];
@@ -39,12 +43,14 @@ namespace Iterative_Greens_Function
 
         void Iterate_Slice(ref Mem[] G_ii, ref Mem[] G_in, ref Mem[] G_ni, Mem Potential, int slice_no, double Energy)
         {
-            // Calculate G_n+1,n+1
+            // Calculate G_n+1,n+1 (MacKinnon 5a)
+            Mem E_minus_H = Calculate_Hamiltonian(Potential, slice_no, Energy);
+            G_ii[slice_no] = Calculate_New_Greens_Function(E_minus_H, G_ii[slice_no - 1]);
             
             // Calculate influence of the new slice on the previously calculated results
-            Calculate_Connections(G_nplusnplus, ref G_ii, ref G_in, ref G_ni, Potential, slice_no, Energy);
+            Calculate_Connections(G_ii[slice_no], ref G_ii, ref G_in, ref G_ni, Potential, slice_no, Energy);
         }
-
+        
         /// <summary>
         /// Calculates the Green's functions from each slice to the end and 
         /// how the new diagonal Green's functions are influenced by the new
@@ -52,11 +58,40 @@ namespace Iterative_Greens_Function
         /// </summary>
         void Calculate_Connections(Mem G_nplusnplus, ref Mem[] G_ii, ref Mem[] G_in, ref Mem[] G_ni, Mem Potential, int slice_no, double Energy)
         {
+            // Calculate G_n+1,i 
+            for (int i = 0; i < slice_no; i++)
+                G_ni[i] = ViennaCL.TripMatProd(G_ii[slice_no], hop_mat_conj, G_ni[i]);   // (MacKinnon 5c)
+
+            // Calculate G_i,i ( note that G_ni has been updated to include the next slice so
+            // it is technically G_{n+1, i} )
+            for (int i = 1; i < slice_no; i++)
+                G_ii[i] = ViennalCL.Add(G_ii[i], ViennalCL.TripProd(G_in[i], hop_mat, G_ni[i]));      // (MacKinnon 5d with i = j)
+                    
             // Calculate G_i,n+1
+            for (int i = 0; i < slice_no + 1; i++)
+                G_in[i] = ViennaCL.TripProd(G_in[i], hop_mat, G_ii[slice_no]);  // (MacKinnon 5d)
+        }
 
-            // Calculate G_i,i
+        Mem Calculate_Hamiltonian(Mem Potential, int slice_no, double Energy)
+        {
+            int slice_start = slice_no * slice_width;
 
-            // Calculate G_n+1,i
+            // Get potential from memory
+            Mem pot_Slice = ViennaCL.GetSlice(Potential, slice_start, slice_start + slice_width, 1);
+            pot_Slice = ViennaCL.Subtract(ViennaCL.Prod(Energy, ViennaCL.UnitVector), pot_Slice);
+
+            // Construct (E - V) - H_0
+            return ViennaCL.Subtract(ViennaCL.Prod(ViennaCL.Identity, pot_Slice), H0_slice);
+        }
+
+        Mem Calculate_New_Greens_Function(Mem E_minus_H, Mem G_nn)
+        {
+            // Calculate V G_nn V\dag
+            Mem result = ViennaCL.TripProd(hop_mat, G_nn, hop_mat_conj);
+            result = ViennaCL.Add(E_minus_H, result);
+
+            // Invert matrix
+            return ViennaCL.Invert(result);
         }
 
         public double Calculate_Currents()
