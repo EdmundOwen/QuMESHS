21a0c6de702b7487b0f8d13e92c220330885f4d0
Edmund Owen <none@none>
2013-06-13 17:56:09 +0100

    Addition of a regular grid Poisson solver

--- OneD_ThomasFermiPoisson/OneD_PoissonSolver.cs
+++ OneD_ThomasFermiPoisson/OneD_PoissonSolver.cs
@@ -2,10 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CenterSpace.NMath.Core;
+using Solver_Bases;
 
 namespace OneD_ThomasFermiPoisson
 {
-    class OneD_PoissonSolver
+    class OneD_PoissonSolver : Potential_Base
     {
+        // parameters for regular grid solve
+        int nz;
+        double dz;
+
+        // 
+        public void Initialise(double dz, int nz)
+        {
+            this.nz = nz; this.dz = dz;
+        }
+
+        public DoubleVector Get_Potential(DoubleVector density)
+        {
+            // calculate potential on a regular grid (not ideal, or scalable)
+            return Get_Potential_On_Regular_Grid(density);
+        }
+
+        DoubleVector Get_Potential_On_Regular_Grid(DoubleVector density)
+        {
+            // generate Laplacian matrix (spin-resolved)
+            DoubleMatrix laplacian = Generate_Laplacian();
+
+            return NMathFunctions.Solve(laplacian, density);
+        }
+
+        /// <summary>
+        /// Generates a spin-resolved laplacian matrix in one-dimension on a regular grid with Dirichlet BCs
+        /// </summary>
+        /// <returns></returns>
+        DoubleMatrix Generate_Laplacian()
+        {
+            DoubleMatrix result = new DoubleMatrix(2 * nz, 2 * nz);
+            for (int i = 0; i < 2 * nz - 1; i++)
+            {
+                // on-diagonal term
+                result[i, i] = 2.0 * epsilon;
+                // off-diagonal
+                result[i + 1, i] = -1.0 * epsilon;
+                result[i, i + 1] = -1.0 * epsilon;
+            }
+            // and delete spin-sector coupling terms 
+            result[nz - 1, nz] = 0.0;
+            result[nz, nz - 1] = 0.0;
+
+            // and fix boundary conditions for spin-up
+            result[0, 0] = 1.0 * epsilon;
+            result[0, 1] = 0.0;
+            result[nz - 1, nz - 1] = 1.0 * epsilon;
+            result[nz - 1, nz - 2] = 0.0;
+
+            // and similarly for spin-down
+            result[nz, nz] = 1.0 * epsilon;
+            result[nz, nz + 1] = 0.0;
+            result[2 * nz - 1, 2 * nz - 1] = 1.0 * epsilon;
+            result[2 * nz - 1, 2 * nz - 2] = 0.0;
+
+            return result / (dz * dz);
+        }
     }
 }
---
+++ Solver_Bases/Potential_Base.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solver_Bases
+{
+    public abstract class Potential_Base
+    {
+        // physical constants
+        protected const double q_e = -0.160217646;             // (aC)
+        protected const double epsilon = 13 * 1.41859713e-6;   // (aC)^2 (nm)^-1 (meV)^-1 for GaAs
+    }
+}
