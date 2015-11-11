35145ae6a8b4d4aa260d5365029c5739c2c95f2d
Edmund <edmundowen@googlemail.com>
2015-11-11 14:23:41 +0000

    Adding example license class in Solver_Bases

---
+++ Solver_Bases/Example_License.cs
@@ -0,0 +1,57 @@
+﻿/***************************************************************************
+ * 
+ * QuMESHS (Quantum Mesoscopic Electronic Semiconductor Heterostructure
+ * Solver) for calculating electron and hole densities and electrostatic
+ * potentials using self-consistent Poisson-Schroedinger solutions in 
+ * layered semiconductors
+ * 
+ * Copyright(C) 2015 E. T. Owen and C. H. W. Barnes
+ * 
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * For additional information, please contact eto24@cam.ac.uk or visit
+ * <http://www.qumeshs.org>
+ * 
+ **************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solver_Bases
+{
+    static public class Example_License
+    {
+        static string nmath_license_key = "";
+
+        static public string NMath_License_Key
+        {
+            get
+            {
+                if (nmath_license_key == "")
+                    throw new Exception("Error - NMath license not set.  An NMath license must be purchased in order to compile this software.");
+                else
+                    return nmath_license_key; 
+            }
+        }
+    }
+}
